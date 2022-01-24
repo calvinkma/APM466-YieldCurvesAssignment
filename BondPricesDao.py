b7d909d1f3c2c53dcc1ebe0daa5af7d61d7c7760
calvinkma <calvinma1999@hotmail.com>
2022-01-23 21:20:27 -0500

    Set up virtual environment.

--- BondPricesDao.py
+++ BondPricesDao.py
@@ -1,1 +1,12 @@
-# Responsible for importing and parsing bond data from the CSV
+import pandas as pd
+
+class BondPricesDao():
+    BOND_PRICES_CSV_FILENAME = "CDNGovtBondPrices.csv"
+
+    def __init__(self):
+        self._load_csv()
+        return
+    
+    def _load_csv(self):
+        df = pd.read_csv(self.BOND_PRICES_CSV_FILENAME)
+        print(df)
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from BondPricesDao import BondPricesDao
+
 def main():
     print("Hello World!")
+    d = BondPricesDao()
     return
 
 if __name__ == "__main__":
