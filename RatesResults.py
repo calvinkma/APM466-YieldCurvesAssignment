fb12511e2619868e3351d7f1b467b779ed5fe194
calvinkma <calvinma1999@hotmail.com>
2022-01-25 23:12:41 -0500

    Add plotting and interpolation.

---
+++ RatesResults.py
@@ -0,0 +1,48 @@
+from datetime import datetime, date
+import matplotlib.pyplot as plt
+from scipy import interpolate
+import numpy as np
+
+class RatesResullts:
+    def __init__(self):
+        self.rates_curve_by_date = {}
+    
+    def record_rates_curve(self, date, x, y):
+        sorted_x, sorted_y = zip(*sorted(zip(x, y)))
+        self.rates_curve_by_date[date] = (sorted_x, sorted_y)
+    
+    def print_all_data(self):
+        for date in self.rates_curve_by_date:
+            print("Rates curve for ", date, " in <date, rate> pairs: ")
+            print(list(zip(self.rates_curve_by_date[date][0], self.rates_curve_by_date[date][1])))
+            print("=====================")
+    
+    def plot(self, xlabel, ylabel, out_filename):
+        cmap = plt.cm.get_cmap("BuPu")
+        colors = cmap(np.linspace(0, 1, len(self.rates_curve_by_date)))
+
+        for date, color in zip(self.rates_curve_by_date, colors):
+            sorted_x, sorted_y = self.rates_curve_by_date[date][0], self.rates_curve_by_date[date][1]
+
+            # Convert datetype x values to float-valued POXIS timestamp for interpolation
+            sorted_x_sec = self._date_to_second(sorted_x)
+            tck = interpolate.splrep(sorted_x_sec, sorted_y)
+
+            # Define intermediate x values where the rate should be estimated
+            sorted_x_sec_interpolated = np.linspace(sorted_x_sec[0], sorted_x_sec[-1], 500)
+            sorted_x_interpolated = self._second_to_date(sorted_x_sec_interpolated)
+            sorted_y_interpolated = interpolate.splev(sorted_x_sec_interpolated, tck)
+
+            plt.plot(sorted_x_interpolated, sorted_y_interpolated, c=color)
+            plt.plot(sorted_x, sorted_y, c=color, marker='x', linestyle='None', label='_nolegend_')
+        
+        plt.legend(self.rates_curve_by_date)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.savefig(out_filename)
+    
+    def _date_to_second(self, dates):
+        return [datetime.combine(i, datetime.min.time()).timestamp() for i in dates]
+    
+    def _second_to_date(self, seconds):
+        return [date.fromtimestamp(second) for second in seconds]
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from BondPricesDao import BondPricesDao
 from BondPricesTransformer import BondPricesTransformer
-from RatesRecorder import RatesRecorder
+from RatesResults import RatesResullts
 from YieldCalculator import YieldCalculator
 from util import get_date_like_column_names
 from Globals import Globals
@@ -22,17 +22,17 @@ def _get_processed_data():
 def main():
     processed_data = _get_processed_data()
 
-    yield_rates_recorder = RatesRecorder()
+    yield_rates_results = RatesResullts()
     yield_calculator = YieldCalculator(processed_data)
 
     price_observation_string_dates = get_date_like_column_names(processed_data)
     for string_date in price_observation_string_dates:
         date = datetime.strptime(string_date, Globals.DATETIME_FORMAT_STR).date()
         yield_x, yield_y = yield_calculator.get_yield_curve_on_date(string_date)
-        yield_rates_recorder.record_rates_curve(date, yield_x, yield_y)
+        yield_rates_results.record_rates_curve(date, yield_x, yield_y)
     
     # Plot
-    yield_rates_recorder.print_all_data()
+    yield_rates_results.plot("Maturity", "YTM", "yield_curves.png")
 
     return
 
