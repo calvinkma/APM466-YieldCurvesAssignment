c3c5b745ef6b98c0c1c1b5b06e607e6e187dc991
calvinkma <calvinma1999@hotmail.com>
2022-01-25 21:28:12 -0500

    Add record and print to RatesRecorder.

--- RatesRecorder.py
+++ RatesRecorder.py
@@ -1,3 +1,12 @@
 class RatesRecorder:
     def __init__(self):
-        self.rates_data = {}
+        self.rates_curve_by_date = {}
+    
+    def record_rates_curve(self, date, x, y):
+        self.rates_curve_by_date[date] = (x, y)
+    
+    def print_all_data(self):
+        for date in self.rates_curve_by_date:
+            print("Rates curve for ", date, " in <date, rate> pairs: ")
+            print(list(zip(self.rates_curve_by_date[date][0], self.rates_curve_by_date[date][1])))
+            print("=====================")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from BondPricesDao import BondPricesDao
 from BondPricesTransformer import BondPricesTransformer
+from RatesRecorder import RatesRecorder
 from YieldCalculator import YieldCalculator
 from util import get_date_like_column_names
 from Globals import Globals
@@ -20,13 +22,17 @@ def _get_processed_data():
 def main():
     processed_data = _get_processed_data()
 
+    yield_rates_recorder = RatesRecorder()
     yield_calculator = YieldCalculator(processed_data)
 
     price_observation_string_dates = get_date_like_column_names(processed_data)
     for string_date in price_observation_string_dates:
+        date = datetime.strptime(string_date, Globals.DATETIME_FORMAT_STR).date()
         yield_x, yield_y = yield_calculator.get_yield_curve_on_date(string_date)
-        # print(string_date, yield_x, yield_y)
-        # Log to RatesRecorder
+        yield_rates_recorder.record_rates_curve(date, yield_x, yield_y)
+    
+    # Plot
+    yield_rates_recorder.print_all_data()
 
     return
 
